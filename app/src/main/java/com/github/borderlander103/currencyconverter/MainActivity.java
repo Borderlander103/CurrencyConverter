0205927d819a61e0be5124ff47c902b9f9594ffc
Borderlander103 <mailbox@brianrichardsononline.com>
2018-10-02 09:15:00 -0700

    Finish app challenge of simpler converter
    Was unable to implement the web scrape of current rate. Currently is fixed amount.

--- app/src/main/java/com/github/borderlander103/currencyconverter/MainActivity.java
+++ app/src/main/java/com/github/borderlander103/currencyconverter/MainActivity.java
@@ -3,7 +3,9 @@ package com.github.borderlander103.currencyconverter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,8 +17,17 @@ public class MainActivity extends AppCompatActivity {
 
     public void convertButton(View view) {
 
+        EditText amountView = (EditText) findViewById(R.id.amountEditText);
 
+        String amountText = amountView.getText().toString();
+
+        double amount = Double.parseDouble(amountText);
+
+        double currentRate = 1.15607;
+
+        String exchangedAmount = String.format("%.2f", (currentRate * amount));
+
+        Toast.makeText(this, "€" + amountText + " is currently $" + exchangedAmount, Toast.LENGTH_LONG).show();
 
     }
-
 }
