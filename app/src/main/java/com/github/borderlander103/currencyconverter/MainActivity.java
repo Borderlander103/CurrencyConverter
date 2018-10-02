4dcca5ffa89f4908440c1a5741bd66094bdfb648
Borderlander103 <mailbox@brianrichardsononline.com>
2018-10-01 20:53:12 -0700

    Initial commit

---
+++ app/src/main/java/com/github/borderlander103/currencyconverter/MainActivity.java
@@ -0,0 +1,22 @@
+package com.github.borderlander103.currencyconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void convertButton(View view) {
+
+
+
+    }
+
+}
